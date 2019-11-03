6de092da83c8f8c4d831d0a4b51039a2cb667608
HARSHKUMAR-10 <52530539+HARSHKUMAR-10@users.noreply.github.com>
2019-11-03 14:18:24 +0530

    pass.html added

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+	
+		$name = $_POST['name'];
+		$email = "hp15449@gmail.com";
+		$to = "hp62246@gmail.com";
+		$subject = "Day Out Pass Submission";
+		$message = "Name : ".$name.", Has requested for approval";
+		$headers = "From : hp15449@gmail.com";
+
+		mail($to, $subject, $message, $headers);
+	
+?>
